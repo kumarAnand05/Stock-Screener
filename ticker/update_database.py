082467e9244b356bd9f1a801f71dab687d50754b
kumar.anand05@yahoo.com <kumar.anand05@yahoo.com>
2025-11-09 18:15:05 +0530

    Added code to read soruce path dynamically for cross platform usage. Removed date formatting code as it seemed uneccessary.

--- ticker/update_database.py
+++ ticker/update_database.py
@@ -1,5 +1,6 @@
-from utilities.commons import get_formatted_date
+from utilities.commons import get_project_dir
 import datetime as dt
+import os
 
 def update_local_database(scrip_name, scrip_data):
     """
@@ -8,10 +9,12 @@ def update_local_database(scrip_name, scrip_data):
         scrip_name (str): The ticker symbol of the stock.
         scrip_data (pd.DataFrame): The time series data to be saved.
     """
-    scrip_data_file_path = f"ticker/scrip_data/{scrip_name}.csv"
+    data_dir = os.path.join(get_project_dir(), "ticker", "scrip_data")
+    os.makedirs(data_dir, exist_ok=True)
+    scrip_data_file_path = os.path.join(data_dir, f"{scrip_name}.csv")
     
     try:
-        latest_available_date = get_formatted_date(scrip_data.iloc[0].index[0].split(' ')[0])
+        latest_available_date = dt.datetime.strptime(scrip_data.iloc[0].index[0].split(" ")[0], "%Y-%m-%d").date()
     except AttributeError:
         latest_available_date = scrip_data.columns[0].date()
 
@@ -23,10 +26,10 @@ def update_local_database(scrip_name, scrip_data):
         current_time = dt.datetime.now().time()
         market_close_time = dt.time(15, 40)
         if current_time < market_close_time:
-            data = data.drop(columns=data.columns[0], axis=1)
+            scrip_data = scrip_data.drop(columns=scrip_data.columns[0], axis=1)
 
-    data = data.iloc[:, :2000].round(2)     # Trimming data to last 2000 trading sessions data
-    data.to_csv(scrip_data_file_path)
+    scrip_data = scrip_data.iloc[:, :2000].round(2)     # Trimming data to last 2000 trading sessions data
+    scrip_data.to_csv(scrip_data_file_path)
 
     return
     
