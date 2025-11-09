d296540b0d9b0a7a662982676be6bf6db266720d
kumar.anand05@yahoo.com <kumar.anand05@yahoo.com>
2025-11-09 16:55:10 +0530

    Added py file to store the scrip symbols that are to be scanned

---
+++ ticker/scrip_names.py
@@ -0,0 +1,11 @@
+"""
+Scrip names that will be scanned by the stock screener.
+"""
+
+SCRIP_NAMES = {
+    "RELIANCE" : "Reliance Industries Limited",
+    "TCS" : "Tata Consultancy Services Limited",
+    "HDFCBANK" : "HDFC Bank Limited",
+    "ICICIBANK" : "ICICI Bank Limited",
+    "BHARTIARTL" : "Bharti Airtel Limited"
+}
