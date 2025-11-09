e34903419cf815e0c77695925e55447530866dbf
kumar.anand05@yahoo.com <kumar.anand05@yahoo.com>
2025-11-09 18:10:29 +0530

    Added logger info for debugging

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from ticker import update_database as ud
 scanned_stocks = 0
 total_stocks = len(SCRIP_NAMES)
 
+print(f"[INFO] Starting stock scanner...Total stocks to scan: {total_stocks}")
+
 scanner_output = []
 
 for scrip in SCRIP_NAMES:
@@ -12,16 +14,18 @@ for scrip in SCRIP_NAMES:
     scanned_stocks += 1
     stock_analysis = []
 
+    print(f"[INFO] Scanning {SCRIP_NAMES.get(scrip)}...")
+    
     try:    
         scrip_data = td.get_scrip_data(scrip)
         ud.update_local_database(scrip, scrip_data)
 
-    except IndexError:
-        print(f"Unable to fetch data for {scrip}. Skipping...")
-        continue
-    except TypeError:
-        print(f"Some error occurred while fetching data for {scrip}. Skipping...")
-        continue
+    except IndexError as i:
+        print(f"[ERROR] Unable to fetch data for {scrip}. Skipping...")
+        print(i)
+    except TypeError as t:
+        print(f"[Error] Some error occurred while fetching data for {scrip}. Skipping...")
+        print(t)
 
-    print(f"Scanned {SCRIP_NAMES.get(scrip)}, {total_stocks - scanned_stocks} remaining.")
+    print(f"[SUCCESS] Scanned {SCRIP_NAMES.get(scrip)}, {total_stocks - scanned_stocks} remaining.")
 
