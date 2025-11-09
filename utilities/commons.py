6bf0ad359855c9b429fdc651bbe24d0ef3106334
kumar.anand05@yahoo.com <kumar.anand05@yahoo.com>
2025-11-09 18:15:50 +0530

    Removed the function to format the datetime object.

--- utilities/commons.py
+++ utilities/commons.py
@@ -1,13 +1,9 @@
-import datetime
+import os
+from datetime import datetime
 
-
-def get_formatted_date(date_obj):
+def get_project_dir():
     """
-    Formats a datetime object to 'YYYY-MM-DD' string format.
-    Args:
-        date_obj (datetime): The datetime object to format.
-    Returns:
-        str: Formatted date string.
+    Returns the root directory of the project.
     """
-    return date_obj.strftime('%Y-%m-%d').date()
-
+    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
